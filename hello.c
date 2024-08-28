fc79d292b6ff91cdfad5e029eb3ad9d5748a2f9b
Seth Obert <sobert2@huskers.unl.edu>
2024-08-28 12:49:23 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ *
+ * Author: Seth Obert
+ * Date: 2024/08/28
+ *
+ * Code that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello my name is Seth!\n");
+
+    return 0;
+}
